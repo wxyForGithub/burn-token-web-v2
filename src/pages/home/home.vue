e0ca21ab45384da7b5ec5aa937b23fba08d878b8
chenjia404 <chenjiablog@gmail.com>
2021-03-16 13:17:36 +0800

    算力大于50就可以升级

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -748,7 +748,7 @@ export default {
       );
       let [error, res] = await this.to(contract.power(this.myAddress));
       this.doResponse(error, res, "oldPower", this.decimals);
-      if (this.oldPower < 100) {
+      if (this.oldPower < 50) {
         this.show_upgrade = false;
       } else {
         this.show_upgrade = true;
