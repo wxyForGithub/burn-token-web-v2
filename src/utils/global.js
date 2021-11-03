358e51cf9e6bc61402890ddd8a9fe834a3a765d2
chenjia404 <chenjiablog@gmail.com>
2021-11-03 19:56:57 +0800

    增加主网支持

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,14 +1,35 @@
 const QKI_CONFIG = {
   chainId: '20181205',
   chainIdHex: '0x133f0d5',
+  chainName:"qki",
+  nativeCurrency:"QKI",
   tipsDesc: '推荐使用qkswap交易，避免被骗。点击进入',
-  tipsUrl: 'https://v3.qkswap.io',
+  tipsUrl: 'https://app.qkswap.io',
   openPluginToast : '请安装metamask插件、或者使用qkpay打开',
   toggleToast : '请使用QKI主网,请切换到QKI主网',
   useToast: '请使用qki主网',
   toggleToast2: '你当前没有使用QKI主网，请切换主网为QKI',
+  rpcUrl:['https://sg.node.quarkblockchain.org'],
+  blockExplorerUrls:['https://qkiscan.io/']
+}
+
+const HECO_CONFIG = {
+  chainId: '128',
+  chainIdHex: '0x80',
+  chainName:"heco",
+  nativeCurrency:"HT",
+  tipsDesc: '推荐使用 mdex 交易，避免被骗。点击进入',
+  tipsUrl: 'https://ht.mdex.co/',
+  openPluginToast : '请安装metamask插件、或者使用qkpay打开',
+  toggleToast : '请使用HECO主网,请切换到HECO主网',
+  useToast: '请使用HECO主网',
+  toggleToast2: '你当前没有使用HECO主网，请切换主网为HECO',
+  rpcUrl:['https://http-mainnet.hecochain.com'],
+  blockExplorerUrls:['https://hecoinfo.com/']
 }
 const GLOBAL_CONFIGS = QKI_CONFIG
 export {
-  GLOBAL_CONFIGS
+  GLOBAL_CONFIGS,
+  QKI_CONFIG,
+  HECO_CONFIG
 };
