1e80c6ffd537fc973349fc7fb6ea3952ac707961
chenjia404 <chenjiablog@gmail.com>
2022-01-10 01:19:30 +0800

    优化编译输出

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ?'./' : '/', // 默认'/'，部署应用包时的基本 URL
+  productionSourceMap:false,
+  outputDir:process.env.VUE_APP_title,
+  integrity:true, //
 }
