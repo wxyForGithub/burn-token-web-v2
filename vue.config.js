65ae8c5a415634e874a8caa6054be702cc739e58
chenjia404 <chenjiablog@gmail.com>
2022-01-10 13:21:02 +0800

    修改名称

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ?'./' : '/', // 默认'/'，部署应用包时的基本 URL
   productionSourceMap:false,
-  outputDir:process.env.VUE_APP_title,
+  outputDir:process.env.VUE_APP_tokenSymbol,
   integrity:true, //
 }
